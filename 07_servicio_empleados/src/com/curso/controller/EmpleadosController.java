987e45f6b02bc67b9ea0a6bee12445ec0322f5cb
Antonio Martín <ajms66@gmail.com>
2020-03-25 10:52:16 +0100

    commit ejercicio 7 completo

--- 07_servicio_empleados/src/com/curso/controller/EmpleadosController.java
+++ 07_servicio_empleados/src/com/curso/controller/EmpleadosController.java
@@ -2,9 +2,15 @@ package com.curso.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.model.Empleado;
@@ -29,4 +35,43 @@ public class EmpleadosController {
 	public List<Empleado> todos(){
 		return empleados;
 	}
+	@GetMapping(value="/empleados/{dep}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Empleado> filtroDepartamento(@PathVariable("dep") String dep){
+		return empleados
+			.stream()
+			.filter(e->e.getDepartamento().equals(dep))
+			.collect(Collectors.toList());
+	}
+	@GetMapping(value="/empleados/{dep}/{salario}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Empleado> filtroDepartamento(@PathVariable("dep") String dep,
+											@PathVariable("salario") double salario){
+		return empleados
+			.stream()
+			.filter(e->(e.getDepartamento().equals(dep)&&e.getSalario()<salario))
+			.collect(Collectors.toList());
+	}
+	
+	
+	@DeleteMapping(value="/empleados/{nombre}")
+	public void eliminarPorNombre(@PathVariable("nombre") String nombre) {
+		empleados.removeIf(e->e.getNombre().equals(nombre));
+	}
+	
+	@PostMapping(value="/empleado",consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void altaEmpleado(@RequestBody Empleado emp) {
+		empleados.add(emp);
+	}
+	
+	@PutMapping(value="/empleado/{nombre}/{salario}")
+	public void actualizaSalario(@PathVariable("nombre") String nombre,
+								 @PathVariable("salario") double salario) {
+		Empleado em=empleados.stream()
+				.filter(e->e.getNombre().equals(nombre))
+				.findFirst()
+				.orElse(null);
+		if(em!=null) {
+			em.setSalario(salario);
+		}
+	}
+	
 }
